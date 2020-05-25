74d25709550b868304627a5cd90b9b75cd4728a5
kyle-law <junwei_1121@hotmail.com>
2020-05-25 15:28:11 +0800

    add GET and POST requests adapter in server.js

--- server.js
+++ server.js
@@ -7,15 +7,48 @@ const express = require('express');
 // Start up an instance of app
 const app = express();
 
+const bodyParser = require('body-parser');
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
 // Cors for cross origin allowance
+const cors = require('cors')
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = process.env.PORT || 3000;
+
+// Spin up the server
+const server = app.listen(port, listening);
+
+// Callback to debug
+function listening(){
+    console.log("server running"); 
+    console.log(`running on localhost: ${port}`);
+}
+
+app.get('/all', getRecord)
+function getRecord(req, res) {
+    res.send(projectData);
+};
+
+app.post('/addJournal',addJournal);
+function addJournal(req,res) {
+    newEntry = {
+        date: req.body.date,
+        weather: req.body.weather,
+        mood: req.body.mood
+    }
+
+    projectData.push(newEntry);
+    // console.log(projectData);
+    res.send(true);
+};
--- website/app.js
+++ website/app.js
@@ -1,46 +1,87 @@
 /* Global Variables */
-const api = {
-    key: "a827ebeb3f600fff6e6558af4d980f98",
-    base: "https://api.openweathermap.org/data/2.5/"
-}
-const searchbox = document.getElementById('zip');
-const submit = document.getElementById('generate')
-
+const baseURL = 'api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '    ';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-submit.addEventListener('click',setQuery);
+document.getElementById('generate').addEventListener('click',performAction);
 
-function setQuery() {
-    console.log(searchbox.value)
-    // getResults(searchbox.value)
+function performAction(e) {
+    const newQuery = document.getElementById('zip').value;
+    // getWeather(baseURL,newQquery, apiKey)
+    const feelings = document.getElementById('feelings').value;
+
+    getWeather('weatherData',)
+    .then(function(data) {
+        console.log(data);
+        postData('/addWeather',{weather:data.weather.main,temp:data.main.temp})
+    })
+    .then(
+        updateUI()
+    )
 }
 
-function getResults (query) {
-    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then(weather => {
-        return weather.json();
-    }).then(displayResults);
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try{
+        const allData = await request.json();
+        document.getElementById('date').innerHTML = newDate;
+        document.getElementById('temp').innerHTML = allData[0].temp;
+        document.getElementById('content').innerHTMl = allData[0].feelings
+    } catch(error) {
+        console.log("error",error);
+    }
 }
 
-function displayResults (weather) {
-    let city = document.querySelector('.location .city');
-    city.innerText = `${weather.name}, ${weather.sys.country}`;
+const getWeather = async (baseURL, query, key) => {
+    const res = await fetch(baseURL+query+key)
+    try {
+        const data = await res.json();
+        console.log(data)
+        return data;
+    } catch(error) {
+        console.log('error',error);
+    }
+}
+
+
+// const searchbox = document.getElementById('zip');
+// const submit = document.getElementById('generate')
+
+
+
+// submit.addEventListener('click',setQuery);
+
+// function setQuery() {
+//     console.log(searchbox.value)
+//     // getResults(searchbox.value)
+// }
+
+// function getResults (query) {
+//     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+//     .then(weather => {
+//         return weather.json();
+//     }).then(displayResults);
+// }
+
+// function displayResults (weather) {
+//     let city = document.querySelector('.location .city');
+//     city.innerText = `${weather.name}, ${weather.sys.country}`;
   
-    let now = new Date();
-    let date = document.querySelector('.location .date');
-    date.innerText = newDate;
+//     let now = new Date();
+//     let date = document.querySelector('.location .date');
+//     date.innerText = newDate;
   
-    let temp = document.querySelector('.current .temp');
-    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+//     let temp = document.querySelector('.current .temp');
+//     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
   
-    let weather_el = document.querySelector('.current .weather');
-    weather_el.innerText = weather.weather[0].main;
+//     let weather_el = document.querySelector('.current .weather');
+//     weather_el.innerText = weather.weather[0].main;
   
-    let hilow = document.querySelector('.hi-low');
-    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
-  }
+//     let hilow = document.querySelector('.hi-low');
+//     hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+//   }
 
 
